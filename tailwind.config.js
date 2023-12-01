23742d4a3e757af536ee900816355a982fb3f018
NelanJoe <nelanjorelat@gmail.com>
2023-12-01 11:56:44 +0700

    ✅ add: color tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,31 @@
 import tailwindTypography from "@tailwindcss/typography";
+import tailwindForm from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue": "#6148FF",
+        "soft-blue": "#489CFF",
+        "light-blue": {
+          100: "#EBF3FC",
+          DEFAULT: "#E8F1FF",
+        },
+        success: "#73CA5C",
+        warning: "#FF0000",
+        danger: "#F9CC00",
+      },
+    },
     fontFamily: {
       sans: ["Poppins", "sans-serif"],
     },
   },
-  plugins: [tailwindTypography],
+  plugins: [
+    tailwindTypography,
+    tailwindForm({
+      strategy: "base",
+    }),
+  ],
 };
