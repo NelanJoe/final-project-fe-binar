a2def05f9e41e388d2d9da025c2674d6e7a1124e
NelanJoe <nelanjorelat@gmail.com>
2023-12-06 22:22:53 +0700

    fix: error page

--- src/views/Error/pages/error.jsx
+++ src/views/Error/pages/error.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <main className="min-h-screen grid place-content-center">
+      <section className="flex flex-col justify-center items-center space-y-8">
+        <h1 className="text-center font-semibold text-8xl text-dark-blue">
+          Oops!
+        </h1>
+        <p className="text-lg font-semibold">Something when wrong...</p>
+        <Link
+          to="/"
+          className="px-4 py-2 rounded-full text-white font-semibold bg-dark-blue hover:bg-dark-blue/80"
+        >
+          Back To Home
+        </Link>
+      </section>
+    </main>
+  );
 };
 
 export default Error;
