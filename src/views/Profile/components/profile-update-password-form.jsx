340ad3c85098a7b8879a570421f0d141fdd14f31
NelanJoe <nelanjorelat@gmail.com>
2024-01-04 17:42:18 +0700

    improvment not change password because google login

--- src/views/Profile/components/profile-update-password-form.jsx
+++ src/views/Profile/components/profile-update-password-form.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 import { useUpdateProfileMutation } from "@/stores";
@@ -10,6 +11,9 @@ import { updatePasswordSchema } from "../validation";
 
 const ProfileUpdatePasswordForm = () => {
   const navigate = useNavigate();
+
+  const { googleLogin } = useSelector((state) => state.auth);
+
   const {
     register,
     handleSubmit,
@@ -50,46 +54,61 @@ const ProfileUpdatePasswordForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="space-y-3">
-        <div className="flex flex-col gap-2">
-          <label htmlFor="password">Masukkan Password Baru</label>
-          <input
-            id="password"
-            type="password"
-            className="w-full rounded-xl"
-            placeholder="*********"
-            {...register("password")}
-          />
-          <span className="text-warning">{errors?.password?.message}</span>
+    <>
+      {googleLogin === true ? (
+        <div className="flex flex-col items-center justify-center my-20 space-y-2">
+          <h2 className="text-lg text-center capitalize text-warning">
+            Tidak dapat merubah password
+          </h2>
+          <Link to="/">
+            <button className="rounded-full btn btn-primary">
+              Back to home
+            </button>
+          </Link>
         </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="confirm-password">Ulangi Password Baru</label>
-          <input
-            id="confirm-password"
-            type="password"
-            className="w-full rounded-xl"
-            placeholder="*********"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          <span className="text-warning">{errorPasswordNotMatch}</span>
-        </div>
-        <div>
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="w-full btn btn-primary rounded-xl"
-          >
-            {isLoading ? (
-              <span className="loading loading-spinner loading-sm"></span>
-            ) : (
-              "Simpan Perubahan"
-            )}
-          </button>
-        </div>
-      </div>
-    </form>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="space-y-3">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="password">Masukkan Password Baru</label>
+              <input
+                id="password"
+                type="password"
+                className="w-full rounded-xl"
+                placeholder="*********"
+                {...register("password")}
+              />
+              <span className="text-warning">{errors?.password?.message}</span>
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="confirm-password">Ulangi Password Baru</label>
+              <input
+                id="confirm-password"
+                type="password"
+                className="w-full rounded-xl"
+                placeholder="*********"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <span className="text-warning">{errorPasswordNotMatch}</span>
+            </div>
+            <div>
+              <button
+                type="submit"
+                disabled={isLoading}
+                className="w-full btn btn-primary rounded-xl"
+              >
+                {isLoading ? (
+                  <span className="loading loading-spinner loading-sm"></span>
+                ) : (
+                  "Simpan Perubahan"
+                )}
+              </button>
+            </div>
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 
